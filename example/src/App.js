25aedb4d7a9c9194256703b508c8c5ae84a346de
unknown <sudarshan.codecraft.co.in>
2021-05-14 16:26:25 +0530

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'cc-custom-button'
+import { Button } from 'cc-custom-button'
 import 'cc-custom-button/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='container'>
+      <Button label='Primary Button' className='btn-primary' />
+      <Button label='Secondary Button' className='btn-secondary' />
+      <Button label='Success Button' className='btn-success' />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
-import styles from './styles.module.css'
+import PropTypes from 'prop-types'
+import styles from './button.module.scss'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export function Button(props) {
+  const { className, disabled, label, type } = props
+  return (
+    <button
+      className={`${styles.btn} ${styles[className]}`}
+      type={type === 'submit' ? 'submit' : 'submit'}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string
+}
+
+Button.defaultProps = {
+  className: styles.btn,
+  disabled: false,
+  type: 'button'
 }
