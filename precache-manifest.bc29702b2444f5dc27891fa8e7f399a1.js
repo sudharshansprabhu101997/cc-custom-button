5506a26c6a0122d127ebf22c2a2f0e0dd022e01b
unknown <sudarshan.codecraft.co.in>
2021-05-14 16:31:27 +0530

    Updates

---
+++ precache-manifest.bc29702b2444f5dc27891fa8e7f399a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70553800985fa9e266b2e95d3132a39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c38e0db6ac19f79ce01",
+    "url": "./static/css/main.4d219600.chunk.css"
+  },
+  {
+    "revision": "72fea39cc6d4dd8d5d4d",
+    "url": "./static/js/2.6aaa0afa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6aaa0afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c38e0db6ac19f79ce01",
+    "url": "./static/js/main.96efc08d.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.96efc08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a5d0bc0213b471c0d2",
+    "url": "./static/js/runtime-main.43244c0b.js"
+  }
+]);
